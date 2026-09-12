2cd21e2add3bc44b9a7c942ab04c47792009d8bb
cangxw <cangxw@msn.com>
2026-09-12 14:54:27 +0900

    TestCommit
    MyTestCommit

--- Assets/Scripts/BattleViewSystem.cs
+++ Assets/Scripts/BattleViewSystem.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class BattleViewSystem
 {
-    
+    public void Initialize()
+    {
+        // Initialization logic for the battle view system
+        Debug.Log("BattleViewSystem initialized.");
+    }
 }
